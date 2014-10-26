5730c11f9b461d4f3ba29fe83d46cb2c2a330882
Luan Chuong <chuong.luan.g@hotmail.com>
2014-10-25 19:29:15 -0700

    updated challenge 2

--- challenge-2/js/app.js
+++ challenge-2/js/app.js
@@ -1,17 +1,15 @@
 // Challenge summary:
-// You will be implementing a simple carousel image gallery.
+// You will be implementing a simple image gallery slideshow.
 // When user clicks on the button associated with each image,
-// that image should transition into the frame.
+// that image should fade into frame.
 
 // First, take a look at the index.html file to see the items you
 // will need to work with.
-// The basic structure is that all images are lined up on one
-// row (each 800px) wide. Overflow:hidden was applied so that you can't 
-// see image that is not in the frame. Essentially, the approach is to
-// move the image into frame.
 
 
 $(function() {
+	generateButtons();
+
 	// Grab all the buttons.
 	var buttons = $(':button');
 
@@ -32,11 +30,43 @@ function setButtonStyle(selectedButton, buttons) {
 
 }
 
+/*
+	Hints:
+		-- To start, get the button's value and look for the associated image in the images object.
+		-- Look up syntax for $.each (foreach function in jQuery).
+		-- Use jQuery fadeIn/fadeOut.
+		-- Remember you can pass in a function to fadeIn/fadeOut. Use it to set attributes during transition.
+*/
+function moveImageIntoFrame(button) {
 
-function moveImageIntoFrame(image) {
-	// Hint: use button id to calculate how many pixels to move image by.
-	// Remember each image is 800px apart, also to parse input before doing arithmetic.
+}
 
 
-	// Hint: use translateX('0px') to move image.
+// HTML output example: 	<button type='button' class='btn btn-primary' value='Skyline'>Skyline</button>
+// 'Active' button has 'btn-danger' instead.
+function generateButtons() {
+	var idx;
+	var button; var buttonStyle;
+	var field = $('#controls');	// Grab the element to append buttons to.
+	var textValue;
+
+	// Loop through images object and create a button for each image.
+	for (idx = 0; idx < images.length; idx++) {
+		button = $(document.createElement('button'));
+
+		// Ternary to switch the first button to 'active'
+		buttonStyle = (idx == 0 ? 'btn btn-danger' : 'btn btn-primary');
+
+		textValue = images[idx].val;
+
+		// Apply attributes to button.
+		button.attr({
+			'type'	: 'button',
+			'class' : buttonStyle,
+			'value'	: textValue
+		});
+
+		button.text(textValue);
+		button.appendTo(field);
+	}
 }
---
+++ challenge-2/js/images.js
@@ -0,0 +1,32 @@
+var images = [
+	{
+		val	: 'Skyline',
+		src	: 'img/seattle_skyline.jpg',
+		alt	: "Seattle's skyline"
+	}, 
+	{
+		val	: 'Night',
+		src	: 'img/night_time.jpg',
+		alt	: "Seattle at night"
+	},
+	{
+		val	: 'Piers',
+		src	: 'img/piers.jpg',
+		alt : "Seattle's piers"
+	}, 
+	{
+		val	: 'Market',
+		src	: 'img/pike_place_market.jpg',
+		alt : "Pike Place Market"
+	}, 
+	{
+		val	: 'Rainier',
+		src	: 'img/rainier.jpg',
+		alt	: "Mt Rainier"
+	}, 
+	{
+		val	: 'Safeco',
+		src	: 'img/safeco_field.jpg',
+		alt	: "SafeCo Field" 
+	}
+]
